ff4bd4e0764ac3d66fbe6908ac01e2cf3d03c345
Biva Candra <biva.candra@alodokter.com>
2022-04-11 17:09:38 +0700

    add tracer

--- lib/tracer.rb
+++ lib/tracer.rb
@@ -2,7 +2,7 @@ module Tracer
   def self.tag_web_param(params)
     current_span = Datadog.tracer.active_span
     params&.each do |key, value|
-      current_span.set_tag("web.param.#{key}", value)
+      current_span.set_tag(key, value)
     end
   end
 end
