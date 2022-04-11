920151a4be56c2172cc69e63d9cf365a8e6d716a
Biva Candra <biva.candra@alodokter.com>
2022-04-11 15:24:41 +0700

    add validation post

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -3,5 +3,6 @@ Datadog.configure do |c|
   app = 'production_demo'
   c.env = 'production'
   c.service = "#{app}-#{Rails.env}"
+  # c.use :rails, service_name: "rails_#{app}_#{Rails.env}", log_injection: true
   c.sampling.default_rate = 1.0
 end
